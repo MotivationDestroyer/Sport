fbe1b6a52cb49026e4510193198abf8557d39f49
Oleg <43904096+MotivationDestroyer@users.noreply.github.com>
2023-09-02 15:47:41 +0300

    сделал тень

--- scripts/scriptModal.js
+++ scripts/scriptModal.js
@@ -1,6 +1,8 @@
 function openModal() {
     document.getElementById("modal").style.top = "-20px";
+    document.getElementById("modal").style.boxShadow="0 331px 0px 0px rgba(43, 53, 65, 0.4)";
 }
 function closeModal() {
     document.getElementById("modal").style.top = "-400px";
+    document.getElementById("modal").style.boxShadow="unset";
 }
