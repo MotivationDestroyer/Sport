955739af4445ac18b0e660d78da7e62d0e36c505
Oleg <43904096+MotivationDestroyer@users.noreply.github.com>
2023-10-16 22:23:55 +0300

    add smooth slider

--- scripts/scriptModal.js
+++ scripts/scriptModal.js
@@ -6,3 +6,16 @@ function closeModal() {
     document.getElementById("modal").style.top = "-400px";
     document.getElementById("modal").style.boxShadow="unset";
 }
+const links = document.querySelectorAll("a");
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+  });
+}
